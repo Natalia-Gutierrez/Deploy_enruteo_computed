f88c07100df49db8250ac2e44e582923559472b9
Natalia <bgutisol2019@gmail.com>
2023-04-28 00:11:37 -0400

    contactos

--- src/components/ContactosView.vue
+++ src/components/ContactosView.vue
@@ -1,4 +1,8 @@
 /* eslint-disable */
 <template>
     <h1> Esta es la pagina de contactos</h1>
+    <h3>Desarrolladora</h3>
+    <p>Natalia Gutierrez</p>
+    <p>Repositorio en github</p>
+    <button href="https://github.com/Natalia-Gutierrez/Deploy_enruteo_computed">repo</button>
 </template>
