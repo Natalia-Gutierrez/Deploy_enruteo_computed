af53365453f24eefa707a4e87b9ea29f62e49cf2
Natalia <bgutisol2019@gmail.com>
2023-04-28 00:16:28 -0400

    estilo ref

--- src/components/ContactosView.vue
+++ src/components/ContactosView.vue
@@ -4,5 +4,5 @@
     <h3>Desarrolladora</h3>
     <p>Natalia Gutierrez</p>
     <p>Repositorio en github</p>
-    <a href="https://github.com/Natalia-Gutierrez/Deploy_enruteo_computed">repo</a>
+    <a href="https://github.com/Natalia-Gutierrez/Deploy_enruteo_computed"  style="text-decoration-color: blueviolet;">repo</a>
 </template>
